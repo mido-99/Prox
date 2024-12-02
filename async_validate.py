b896d129cb0819d2ff6ba3016ba673233e450d97
mido <midohany910@gmail.com>
2024-12-02 16:53:49 +0200

    Use asyncio.Semaphore() for limiting concurrency

--- async_validate.py
+++ async_validate.py
@@ -1,9 +1,13 @@
 import httpx
 import asyncio
 import json
+import time
 
 #? Before you start; refresh your proxy list by visiting: https://proxyscrape.com/free-proxy-list
 #? as most proxies would be dead by the next time you come here
+
+
+semaphore = asyncio.Semaphore(500)
 
 with open('proxies.txt', 'r') as file:
     '''Load & prepare proxies in scheme http://ip:port from proxies.txt'''    
@@ -15,21 +19,24 @@ with open('proxies.txt', 'r') as file:
 async def check_proxy(proxy):
     '''Test each proxy against https://ipinfo.io/json'''
 
-    proxies={
-        'http://': proxy,
-        'https://': proxy,
-        }
-    
-    try:
-        async with httpx.AsyncClient(proxies=proxies, timeout=10) as client:
-            r = await client.get('https://ipinfo.io/json')
-            
-            if r.status_code == 200:
-                print('Valid: ', proxy, '-----', r.text)
-                return proxy
-    except:
-        print('Error: ', proxy)
-        return None
+    async with semaphore:
+        proxies={
+            'http://': proxy,
+            'https://': proxy,
+            }
+        
+        try:
+            async with httpx.AsyncClient(proxies=proxies, timeout=10) as client:
+                r = await client.get('https://ipinfo.io/json')
+                r2 = await client.get('https://httpbin.org/ip')
+                r3 = await client.get('http://lumtest.com/myip.json')
+
+                if (r.status_code + r2.status_code + r3.status_code) == 600:
+                    print('Valid: ', proxy, '-----', r.text)
+                    return proxy
+        except:
+            print('Error: ', proxy)
+            return None
 
 
 async def main():
@@ -44,4 +51,8 @@ async def main():
         with open('valid.txt', 'w') as file:
             file.writelines(f'{line}\n' for line in valid)
 
+time1 = time.perf_counter()
 asyncio.run(main())
+time2 = time.perf_counter()
+
+print('Execution time: ', time2 - time1)
